a6c276085585434ca7db23dcf50b3b518464ef1a
ben-dh3 <benjaminduffield1997@gmail.com>
2024-06-12 17:04:36 +0100

    add training data

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -7,7 +7,7 @@ export async function POST(req: Request, res: NextResponse) {
     const body = await req.json()
   
     const completion = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo",
+      model: "ft:gpt-3.5-turbo-0125:personal::9ZJKxLMn",
       messages: body.messages,
     });
     console.log(completion.choices[0].message);
